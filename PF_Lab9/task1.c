7eb1703ab568f45a3a43407671b9c1f0a87716f9
k250143-netizen <k250143@nu.edu.pk>
2025-11-13 20:40:50 +0500

    Edit

--- PF_Lab9/task1.c
+++ PF_Lab9/task1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void inputData(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
+void inputData(int arr[], int size){
+    for (int i = 0; i < size; i++){
         printf("Enter patients on Day %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 }
 
-float averagePatients(int arr[], int size) {
+float averagePatients(int arr[], int size){
     int sum = 0;
     for (int i = 0; i < size; i++)
         sum += arr[i];
     return (float)sum / size;
 }
 
-int busiestDay(int arr[], int size) {
+int busiestDay(int arr[], int size){
     int maxIndex = 0;
     for (int i = 1; i < size; i++)
         if (arr[i] > arr[maxIndex])
@@ -22,7 +22,7 @@ int busiestDay(int arr[], int size) {
     return maxIndex;
 }
 
-int slowestDay(int arr[], int n) {
+int slowestDay(int arr[], int n){
     int minIndex = 0;
     for (int i = 1; i < n; i++)
         if (arr[i] < arr[minIndex])
@@ -30,7 +30,7 @@ int slowestDay(int arr[], int n) {
     return minIndex;
 }
 
-int main() {
+int main(){
     int patients[7];
     inputData(patients, 7);
 
